6d4ae2540a4a86c7251a0664f76b671f909929ca
dlxisbaby <hebe19880814@163.com>
2017-07-03 17:39:11 +0800

    123

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//app.use(express.static('js'));
 
 
 app.get('/', function(req, res){
@@ -10,26 +9,22 @@ app.get('/', function(req, res){
 });
 
 io.on('connection',function(socket){
-	console.log("a user connected");
+	console.log("a client connected");
 	
 	socket.on('connection',function(data){
 		socket.broadcast.emit('broad',data + " login");
+		socket.on('disconnect',function(){
+			socket.broadcast.emit('broad',data + " log out");
+		});
 	});
 	
 	socket.on('chat message',function(data){
 		io.emit("chat message",data);
-//		console.log('data: '+ JSON.stringify(data));
-//		console.log('data: '+ data.nickname);
 	});
 	
-	socket.on('disconnect',function(data){
-		socket.broadcast.emit('broad',data + " log out");
-		console.log(data);
-	});
 });
 
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
-//http.listen(3000)
